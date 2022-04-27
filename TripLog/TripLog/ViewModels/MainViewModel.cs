33c1a70be73a038c4c063b6a0821821515d8ef65
Paul de Mast <pjfmast@gmail.com>
2022-04-27 22:29:05 +0200

    Add project files.

---
+++ TripLog/TripLog/ViewModels/MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using TripLog.Models;
+
+namespace TripLog.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        ObservableCollection<TripLogEntry> _logEntries;
+        public ObservableCollection<TripLogEntry> LogEntries
+        {
+            get => _logEntries;
+            set
+            {
+                _logEntries = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            LogEntries = new ObservableCollection<TripLogEntry>
+            {
+
+            new TripLogEntry {
+                Title = "Grote Kerk Breda",
+                Notes = "gebouwd 1410 - 1547, 97 meter hoog",
+                Rating = 4,
+                Date = new DateTime(2018, 5, 21),
+                Latitude = 51.588889,
+                Longitude = 4.775278
+            },
+            new TripLogEntry {
+                Title = "Begijnhof Breda",
+                Notes = "13e eeuws, naast stadpark valkenberg",
+                Rating = 3,
+                Date = new DateTime(2019, 2, 28),
+                Latitude = 51.588889,
+                Longitude = 4.775278
+            },
+            new TripLogEntry {
+                Title = "Mezz popppodium",
+                Rating = 4,
+                Notes = "Het poppodium bestaat uit een verbouwde officiersmess uit 1899 en een koperen schelpvormige uitbreiding. De Mezz is de opvolger van poppodium Para.",
+                Date = new DateTime(2020, 3, 6),
+                Latitude = 51.583824,
+                Longitude = 4.778921
+            },
+            new TripLogEntry {
+                Title = "Chasse theater en cinema",
+                Rating = 4,
+                Notes = "Modern gebouw met een opvallend golvend dak naar een ontwerp van Herman Hertzberger",
+                Date = new DateTime(2019, 12, 6),
+                Latitude = 51.5875,
+                Longitude = 4.7822
+            },
+            new TripLogEntry {
+                Title = "Avans Hogeschoollaan",
+                Rating = 4,
+                Notes = "De place to eat-sleep-learn-repeat",
+                Date = new DateTime(2022, 4, 20),
+                Latitude = 51.5835,
+                Longitude = 4.7964
+            },
+            new TripLogEntry {
+                Title = "Pathe Cinema",
+                Rating = 3,
+                Notes = "met relax stoelen",
+                Date = new DateTime(2020, 4, 20),
+                Latitude = 51.5897,
+                Longitude = 4.7850
+            },
+            new TripLogEntry {
+                Title = "T-Huis Breda, Valkenbergpark",
+                Rating = 4,
+                Notes = "Het opvallend gekleurde paviljoen ontworpen door Eindhovense kunstenaar John Körmeling",
+                Date = new DateTime(2021, 5, 1),
+                Latitude = 51.59200,
+                Longitude = 4.78081
+            }
+        };
+        }
+    }
+}
---
+++ TripLog/TripLog/ViewModels/NewEntryViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using TripLog.Models;
+using Xamarin.Forms;
+
+namespace TripLog.ViewModels
+{
+    public class NewEntryViewModel : BaseViewModel
+    {
+        string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                //Validate(() => !string.IsNullOrWhiteSpace(_title), "Title must be provided.");
+                OnPropertyChanged();
+                SaveCommand.ChangeCanExecute();
+            }
+        }
+
+        double _latitude;
+        public double Latitude
+        {
+            get => _latitude;
+            set
+            {
+                _latitude = value;
+                OnPropertyChanged();
+            }
+        }
+
+        double _longitude;
+        public double Longitude
+        {
+            get => _longitude;
+            set
+            {
+                _longitude = value;
+                OnPropertyChanged();
+            }
+        }
+
+        DateTime _date;
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        int _rating;
+        public int Rating
+        {
+            get => _rating;
+            set
+            {
+                _rating = value;
+                //Validate(() => _rating >= 1 && _rating <= 5, "Rating must be between 1 and 5.");
+                OnPropertyChanged();
+                SaveCommand.ChangeCanExecute();
+            }
+        }
+
+        string _notes;
+        public string Notes
+        {
+            get => _notes;
+            set
+            {
+                _notes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        Command _saveCommand;
+        public Command SaveCommand =>
+            _saveCommand ?? (_saveCommand = new Command(Save, CanSave));
+
+        public NewEntryViewModel()
+        {
+            Date = DateTime.Today;
+            Rating = 1;
+        }
+
+        void Save()
+        {
+            var newItem = new TripLogEntry
+            {
+                Title = Title,
+                Latitude = Latitude,
+                Longitude = Longitude,
+                Date = Date,
+                Rating = Rating,
+                Notes = Notes
+            };
+
+            // TODO: Persist entry in a later chapter
+        }
+
+        // Initially CanSave for e New entry should be false, because Title is null (first condition)
+        // After editing HasErrors contains possible validation errors
+        bool CanSave() => !string.IsNullOrWhiteSpace(Title) /*&& !HasErrors*/;
+    }
+}
