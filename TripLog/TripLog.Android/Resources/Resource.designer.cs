2ee915b17beb0be03d7284f727aae094320a0830
Paul de Mast <pjfmast@gmail.com>
2022-04-28 10:28:13 +0200

    Add IValueConverter for converting star rating to star image

--- TripLog/TripLog.Android/Resources/Resource.designer.cs
+++ TripLog/TripLog.Android/Resources/Resource.designer.cs
@@ -22250,14 +22250,29 @@ namespace TripLog.Droid
 			// aapt resource value: 0x7F0700A1
 			public const int preference_list_divider_material = 2131165345;
 			
-			// aapt resource value: 0x7F0700A2
-			public const int test_custom_background = 2131165346;
-			
 			// aapt resource value: 0x7F0700A3
-			public const int tooltip_frame_dark = 2131165347;
+			public const int stars_2 = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int tooltip_frame_light = 2131165348;
+			public const int stars_3 = 2131165348;
+			
+			// aapt resource value: 0x7F0700A5
+			public const int stars_4 = 2131165349;
+			
+			// aapt resource value: 0x7F0700A6
+			public const int stars_5 = 2131165350;
+			
+			// aapt resource value: 0x7F0700A2
+			public const int star_1 = 2131165346;
+			
+			// aapt resource value: 0x7F0700A7
+			public const int test_custom_background = 2131165351;
+			
+			// aapt resource value: 0x7F0700A8
+			public const int tooltip_frame_dark = 2131165352;
+			
+			// aapt resource value: 0x7F0700A9
+			public const int tooltip_frame_light = 2131165353;
 			
 			static Drawable()
 			{
