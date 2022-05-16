06492075b041bee659f9879f04e84fcb148fca6d
Paul de Mast <pjfmast@gmail.com>
2022-05-16 11:32:37 +0200

    update packages add package Microsoft.Extensions.DependencyInjection add class DependencyInjectionContainer

---
+++ TripLog/TripLog/DependencyInjectionContainer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+using TripLog.ViewModels;
+using Xamarin.Forms;
+
+namespace TripLog
+{
+    public static class DependencyInjectionContainer
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            //services.AddSingleton<IMyService, MyService>
+            return services;
+        }
+
+        public static IServiceCollection ConfigureViewModels(this IServiceCollection services)
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            foreach (var type in currentAssembly.DefinedTypes
+                .Where(e => e.IsSubclassOf(typeof(Page))
+                         || e.IsSubclassOf(typeof(BaseViewModel))))
+            {
+                services.AddTransient(type.AsType());
+            }
+            return services;
+        }
+    }
+}
